f998938641e04ae366d4f87ee4dd946db45664d3
Manu Viswambharan <manu_viswambharan@external.mckinsey.com>
2015-05-07 19:52:50 +0530

    Initial commit

---
+++ NancyApplication1/IndexModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace NancyApplication1
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        private Dictionary<string,int> usernameSbDictionary = new Dictionary<string, int>(); 
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+
+            Put["/hello"] = parameters =>
+            {
+                return this.Request.Query["foo"];
+            };
+
+            Get["/api/{username}"] = parameters => usernameSbDictionary[parameters.username];
+
+            Put["/api/{username}"] = parameters =>
+            {
+                usernameSbDictionary.Add(parameters.username,(int) this.Request.Query["SB"]);
+                return "OK";
+            };
+        }
+    }
+}
