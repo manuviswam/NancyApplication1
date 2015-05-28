db79ffccf4a9c3fb3483d3cccf05c72b347b7a54
Harinee <harinee.muralinath@external.mckinsey.com>
2015-05-28 18:18:19 +0530

    Added pages to the framework

---
+++ GaugeFunctionalTests/Pages/RequestPage.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp;
+
+namespace NancyFunctionalE2ETests.StepDefinition
+{
+    public class RequestPage
+    {
+        const string baseUrl = "http://localhost:3579";
+        RestClient client = new RestClient(baseUrl);
+        RestRequest request { get; set; }
+
+        public RequestPage Put(string apiName)
+        {
+            request = new RestRequest(apiName, Method.PUT);
+            return this;
+        }
+
+        public RequestPage Get(string apiName)
+        {
+            request = new RestRequest(apiName, Method.GET);
+            return this;
+        }
+
+        public RequestPage QueryParameter(string parameterName, string parameterValue)
+        {
+            request.AddQueryParameter(parameterName, parameterValue);
+            return this;
+        }
+
+        public RestResponse Execute()
+        {
+            var response = (RestResponse) client.Execute(request);
+            return response;
+        }
+        
+    }
+}
---
+++ GaugeFunctionalTests/Pages/StoryboardApiPage.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+
+namespace NancyFunctionalE2ETests.StepDefinition
+{
+    public class StoryboardApiPage : RequestPage
+    {
+        private const string ApiName = "/api/";
+
+        RestRequest request { get; set; }
+
+        public RestResponse PutStoryboardValueForUser(string username, string numberOfStoryboards)
+        {
+            var api = ApiName + username;
+            var response = Put(api).QueryParameter("SB", numberOfStoryboards).Execute();
+            return response;
+        }
+
+        public string GetStoryboardNumberForUser(string username)
+        {
+            var api = ApiName + username;
+            var response = Get(api).Execute();
+            return response.Content;
+        }
+    }
+}
